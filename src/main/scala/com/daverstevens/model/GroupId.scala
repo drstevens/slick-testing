dcdbf569d441de8888bf7b03ac186605cc106b3a
Dave Stevens <dave@spindle.com>
2014-09-10 23:57:42 -0400

    Attempt to use Slick column extension methods against non-standard column types

--- src/main/scala/com/daverstevens/model/GroupId.scala
+++ src/main/scala/com/daverstevens/model/GroupId.scala
@@ -1,8 +1,15 @@
 package com.daverstevens.model
 
+import scalaz.{Store, @>, Lens}
+
 case class GroupId(value: Long)
 case class RoleId(value: Long)
 
-case class Group(id: GroupId, name: String)
-case class Role(id: RoleId, name: String)
+case class GroupName(value: String)
+case class Group(id: GroupId, name: GroupName, someString: String, someInt: Int)
+object Group {
+  val idLens: Group @> GroupId = Lens(g => Store(gId => g.copy(id = gId), g.id))
+}
+case class RoleName(value: String)
+case class Role(id: RoleId, name: RoleName)
 case class GroupRole(groupId: GroupId, roleId: RoleId)
