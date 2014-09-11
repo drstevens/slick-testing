f484bc2df014a8ccc1a27d8ebeaeea367e717d71
Dave Stevens <dave@spindle.com>
2014-09-11 00:03:24 -0400

    Revert "Revert "Attempt to use Slick column extension methods against non-standard column types""
    This reverts commit cd30aca35d4623730f54dce0d3d16aaf0c03b5ff.

--- src/main/scala/com/daverstevens/sql/SqlModel.scala
+++ src/main/scala/com/daverstevens/sql/SqlModel.scala
@@ -17,7 +17,9 @@ sealed trait SqlModel extends SlickDriver {
 
   object implicitMappings {
     implicit def groupIdMapper = MappedColumnType.base[GroupId, Long](_.value,v => GroupId(v))
+    implicit def groupNameMapper = MappedColumnType.base[GroupName, String](_.value,v => GroupName(v))
     implicit def roleIdMapper = MappedColumnType.base[RoleId, Long](_.value,v => RoleId(v))
+    implicit def roleNameMapper = MappedColumnType.base[RoleName, String](_.value,v => RoleName(v))
   }
 
   import implicitMappings._
@@ -31,25 +33,29 @@ sealed trait SqlModel extends SlickDriver {
 
   def dropTables()(implicit session: Session): Unit = ddl.drop
 
-  class GroupTable(tag: Tag) extends Table[(GroupId, String)](tag, "Group") {
+  class GroupTable(tag: Tag) extends Table[Group](tag, "Group") {
     def id = column[GroupId]("ID", O.AutoInc, O.PrimaryKey)
-    def name = column[String]("NAME", O.NotNull)
+    def name = column[GroupName]("NAME", O.NotNull)
+    def someString = column[String]("SOMESTRING", O.NotNull)
+    def someInt = column[Int]("SOMEINT", O.NotNull)
 
-    def * = (id, name)
+    def * = (id, name, someString, someInt) <>((Group.apply _).tupled, Group.unapply)
   }
   object groupTable extends TableQuery[GroupTable](new GroupTable(_)) {
-    def foo(id: GroupId) = groupTable.filter(_.id === id)
-    def insertAndGet(name: String)(implicit s: Session): Group = Group((groupTable returning groupTable.map(_.id)).insert(GroupId(1l), name), name)
+    def insertAndGet(name: GroupName, someString: String, someInt: Int)(implicit s: Session): Group = {
+      val g = Group(GroupId(1l), name, someString, someInt)
+      val id = (groupTable returning groupTable.map(_.id)).insert(g)
+      Group.idLens.set(g, id)
+    }
   }
 
-  class RoleTable(tag: Tag) extends Table[(RoleId, String)](tag, "Role") {
+  class RoleTable(tag: Tag) extends Table[(RoleId, RoleName)](tag, "Role") {
     def id = column[RoleId]("ID", O.AutoInc, O.PrimaryKey)
-    def name = column[String]("NAME", O.NotNull)
+    def name = column[RoleName]("NAME", O.NotNull)
 
     def * = (id, name)
   }
   object roleTable extends TableQuery[RoleTable](new RoleTable(_)) {
-    def foo(id: RoleId) = roleTable.filter(_.id === id)
   }
 
   class GroupRoleTable(tag: Tag) extends Table[(GroupId, RoleId)](tag, "GroupRole") {
--- src/test/scala/com/daverstevens/sql/H2ModelTests.scala
+++ src/test/scala/com/daverstevens/sql/H2ModelTests.scala
@@ -1,27 +1,27 @@
 package com.daverstevens.sql
 
+import scala.slick.lifted.StringColumnExtensionMethods
 import org.specs2.mutable.Specification
-import com.daverstevens.model.Group
-
+import com.daverstevens.model._
 
 class H2ModelTests extends Specification with H2Testing {
 
   "Insert some data" ! usingH2DB {
     db => implicit model => db.withSession { implicit s =>
-      val exGroup1 = model.groupTable.insertAndGet("group1")
-      val exGroup2 = model.groupTable.insertAndGet("group2")
-      val exGroup3 = model.groupTable.insertAndGet("group3")
+      val exGroup1 = model.groupTable.insertAndGet(GroupName("group1"), "1", 1)
+      val exGroup2 = model.groupTable.insertAndGet(GroupName("group2"), "2", 2)
+      val exGroup3 = model.groupTable.insertAndGet(GroupName("group3"), "3", 1)
 
       import model.driver.simple._
       import model.implicitMappings._
 
-      val group1 = model.groupTable.filter(_.id === exGroup1.id).firstOption.map(Group.tupled)
-      val group2 = model.groupTable.filter(_.id === exGroup2.id).firstOption.map(Group.tupled)
-      val group3 = model.groupTable.filter(_.id === exGroup3.id).firstOption.map(Group.tupled)
+      val group1 = model.groupTable.filter(_.id === exGroup1.id).firstOption
+      val group2 = model.groupTable.filter(_.id === exGroup2.id).firstOption
+      val group3 = model.groupTable.filter(_.id === exGroup3.id).firstOption
 
-      exGroup1.name ==== "group1" and
-      exGroup2.name ==== "group2" and
-      exGroup3.name ==== "group3" and
+      exGroup1.name ==== GroupName("group1") and
+      exGroup2.name ==== GroupName("group2") and
+      exGroup3.name ==== GroupName("group3") and
       (exGroup1.id !=== exGroup2.id) and
       (exGroup2.id !=== exGroup3.id) and
       group1 ==== Some(exGroup1)
@@ -30,18 +30,38 @@ class H2ModelTests extends Specification with H2Testing {
     }
   }
 
+  "Use extensions" ! usingH2DB {
+    db => implicit model => db.withSession { implicit s =>
+      val exGroup1 = model.groupTable.insertAndGet(GroupName("group1"), "str", 10)
+
+      import model.driver.simple._
+      import model.implicitMappings._
+
+      //implicit def stringColumnExtensionMethods(c: Column[GroupName]) = new StringColumnExtensionMethods[GroupName](c)
+      // Uncommenting above line results in the following error
+      // [error] /Users/dave/workspace/slick-testing/src/test/scala/com/daverstevens/sql/H2ModelTests.scala:42: Cannot perform option-mapped operation
+      // [error]       with type: (com.daverstevens.model.GroupName, com.daverstevens.model.GroupName) => R
+      // [error]   for base type: (String, String) => Boolean
+
+      val group1 = model.groupTable.filter(_.name like GroupName("group1")).firstOption
+
+      exGroup1.name ==== GroupName("group1") and
+        group1 ==== Some(exGroup1)
+    }
+  }
+
   "Insert some more data" ! usingH2DB {
     db => implicit model => db.withSession { implicit s =>
-      val exGroup1 = model.groupTable.insertAndGet("group1")
-      val exGroup2 = model.groupTable.insertAndGet("group2")
-      val exGroup3 = model.groupTable.insertAndGet("group3")
+      val exGroup1 = model.groupTable.insertAndGet(GroupName("group1"), "1", 1)
+      val exGroup2 = model.groupTable.insertAndGet(GroupName("group2"), "2", 2)
+      val exGroup3 = model.groupTable.insertAndGet(GroupName("group3"), "3", 1)
 
       import model.driver.simple._
       import model.implicitMappings._
 
       val groups = model.groupTable
         .filter(g => g.id === exGroup1.id || g.id === exGroup2.id || g.id === exGroup3.id)
-        .list.map(Group.tupled)
+        .list
       groups must containAllOf(List(exGroup1, exGroup2, exGroup3))
     }
   }
